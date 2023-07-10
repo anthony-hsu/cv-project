5d2cec8298e042c1a07f7973d16d096977cac804
Anthony Hsu <129446589+anthony-hsu@users.noreply.github.com>
2023-07-10 14:45:19 -0700

    Refactor Education section

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,43 +1,31 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import EducationItem from "./EducationItem";
 import uniqid from "uniqid";
 
-class Education extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      educationItems: [{ id: uniqid() }],
-    };
-  }
-
-  addEducation = () => {
-    this.setState((prevState) => ({
-      educationItems: [...prevState.educationItems, { id: uniqid() }],
-    }),
-    () => console.log(this.state));
+const Education = (props) => {
+  const [educationItems, setEducationItems] = useState([{ id: uniqid() }]);
+  const addEducation = () => {
+    setEducationItems([...educationItems, { id: uniqid() }]);
   };
+  const { editMode } = props;
 
-  render() {
-    const { editMode } = this.props;
-    return (
-      <>
-        <form>
-          <h3>Education</h3>
-          {this.state.educationItems.map((item) => {
-            return <EducationItem key={item.id} editMode={editMode} />;
-          })}
-        </form>
-        {!editMode ? (
-          <></>
-        ) : (
-          <button className="btn-add" onClick={this.addEducation}>
-            +
-          </button>
-        )}
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <form>
+        <h3>Education</h3>
+        {educationItems.map((item) => {
+          return <EducationItem key={item.id} editMode={editMode} />;
+        })}
+      </form>
+      {!editMode ? (
+        <></>
+      ) : (
+        <button className="btn-add" onClick={addEducation}>
+          +
+        </button>
+      )}
+    </>
+  );
+};
 
 export default Education;
--- src/components/EducationItem.js
+++ src/components/EducationItem.js
@@ -1,68 +1,67 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-class EducationItem extends Component {
-  constructor(props) {
-    super(props);
+const EducationItem = (props) => {
+  const [school, setSchool] = useState("The University of Texas at Austin");
+  const [major, setMajor] = useState("B.S., Biomedical Engineering");
+  const [start, setStart] = useState("Fall 2012");
+  const [end, setEnd] = useState("Spring 2016");
+  const { editMode } = props;
+  const mode = editMode ? "editMode" : "submittedMode";
 
-    this.state = {
-      school: "The University of Texas at Austin",
-      major: "B.S., Biomedical Engineering",
-      start: "Fall 2012",
-      end: "Spring 2016",
-    };
-  }
-
-  onInputChange = (e) => {
-    this.setState({
-      [e.target.name]: e.target.value,
-    });
+  const onSchoolInputChange = (e) => {
+    setSchool(e.target.value);
+  };
+  const onMajorInputChange = (e) => {
+    setMajor(e.target.value);
+  };
+  const onStartInputChange = (e) => {
+    setStart(e.target.value);
+  };
+  const onEndInputChange = (e) => {
+    setEnd(e.target.value);
   };
 
-  render() {
-    const { editMode } = this.props;
-    const mode = editMode ? "editMode" : "submittedMode";
-    return (
-      <div className="ed-item-wrapper">
-        <div className="ed-info-wrapper">
-          <input
-            className={`input-school ${mode}`}
-            type="text"
-            name="school"
-            value={this.state.school}
-            onChange={this.onInputChange}
-            disabled={!editMode}
-          />
-          <input
-            className={`input-major ${mode}`}
-            type="text"
-            name="major"
-            value={this.state.major}
-            onChange={this.onInputChange}
-            disabled={!editMode}
-          />
-        </div>
-        <div className="date-wrapper">
-          <input
-            className={`date-start ${mode}`}
-            type="text"
-            name="start"
-            value={this.state.start}
-            onChange={this.onInputChange}
-            disabled={!editMode}
-          />
-          -
-          <input
-            className={`date-end ${mode}`}
-            type="text"
-            name="end"
-            value={this.state.end}
-            onChange={this.onInputChange}
-            disabled={!editMode}
-          />
-        </div>
+  return (
+    <div className="ed-item-wrapper">
+      <div className="ed-info-wrapper">
+        <input
+          className={`input-school ${mode}`}
+          type="text"
+          name="school"
+          value={school}
+          onChange={onSchoolInputChange}
+          disabled={!editMode}
+        />
+        <input
+          className={`input-major ${mode}`}
+          type="text"
+          name="major"
+          value={major}
+          onChange={onMajorInputChange}
+          disabled={!editMode}
+        />
       </div>
-    );
-  }
-}
+      <div className="date-wrapper">
+        <input
+          className={`date-start ${mode}`}
+          type="text"
+          name="start"
+          value={start}
+          onChange={onStartInputChange}
+          disabled={!editMode}
+        />
+        -
+        <input
+          className={`date-end ${mode}`}
+          type="text"
+          name="end"
+          value={end}
+          onChange={onEndInputChange}
+          disabled={!editMode}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default EducationItem;
